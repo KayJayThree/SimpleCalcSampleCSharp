3af609f118566ed24d330b30713daf0616f9af95
Ron Stengel <rkstengel@gmail.com>
2016-09-12 11:59:46 -0700

    Second Push...

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//This is my first run at testing GITHUB and pushing a simple calc to the Eden Branch in the cloud.
+//see how well it works, or maybe not so much...
+// 9/12/2016  happy days
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
